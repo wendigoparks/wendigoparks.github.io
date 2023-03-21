07e6c897e9693f18d83c878ca5066f5fc0e670cc
brhuebsch <brhuebsch@vt.edu>
2023-03-21 15:28:36 -0400

    [backend] create more API endpoints

--- backend/src/crud.py
+++ backend/src/crud.py
@@ -21,3 +21,33 @@ def create_park(db: Session, park: schemas.ParkCreate):
     db.commit()
     db.refresh(db_park)
     return db_park
+
+def find_park(db: Session, park_name: str):
+    return db.query(models.Park).where(models.Park.name == park_name).first()
+
+def search_parks_by_name(db: Session, park_name: str):
+    search = "%{}%".format(park_name)
+    return db.query(models.Park).where(models.Park.name.like(search)).all()
+
+def update_park(db: Session, park_name: str, park: schemas.ParkCreate):
+    db_park = db.query(models.Park).where(models.Park.name == park_name).first()
+    if not db_park:
+        return None
+    db_park.name = park.name,
+    db_park.description = park.description,
+    db_park.address = park.address,
+    db_park.amenities = park.amenities,
+    db_park.phone_nr = park.phone_nr,
+    db_park.capacity = park.capacity,
+    db_park.image_url = park.image_url,
+    db.commit()
+    db.refresh(db_park)
+    return db_park
+
+def delete_park(db: Session, park_name: str):
+    db_park = db.query(models.Park).where(models.Park.name == park_name).first()
+    if not db_park:
+        return None
+    db.delete(db_park)
+    db.commit()
+    return db_park
--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 load_dotenv('.env.local')
 load_dotenv()
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
@@ -22,17 +23,45 @@ def get_db():
         db.close()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.post("/parks/", response_model=schemas.Park)
-def create_park(park: schemas.ParkCreate, db: Session = Depends(get_db)):
-    return crud.create_park(db=db, park=park)
+@app.get("/", include_in_schema=False)
+def redirect_to_docs():
+    return RedirectResponse(url="/docs")
 
 
 @app.get("/parks/", response_model=list[schemas.Park])
 def read_parks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    parks = crud.get_parks(db, skip=skip, limit=limit)
+    parks = crud.get_parks(db=db, skip=skip, limit=limit)
     return parks
+
+@app.get("/park/{park_name}", response_model=schemas.Park)
+def find_park(park_name: str, db: Session = Depends(get_db)):
+    park = crud.find_park(db=db, park_name=park_name)
+    if not park:
+        raise HTTPException(status_code=404, detail="Park not found")
+    return park
+
+@app.get("/parks/{park_name_contains}", response_model=list[schemas.Park])
+def search_parks_by_name(park_name: str, db: Session = Depends(get_db)):
+    parks = crud.search_parks_by_name(db=db, park_name=park_name)
+    return parks
+
+@app.post("/park/", response_model=schemas.Park)
+def create_park(park: schemas.ParkCreate, db: Session = Depends(get_db)):
+    existing_park = crud.find_park(db=db, park_name=park.name)
+    if existing_park:
+        raise HTTPException(status_code=400, detail="Park with this name already exists")
+    return crud.create_park(db=db, park=park)
+
+@app.put("/park/update/{park_name}", response_model=schemas.Park)
+def update_park(park_name: str, park: schemas.ParkCreate, db: Session = Depends(get_db)):
+    existing_park = crud.update_park(db=db, park_name=park_name, park=park)
+    if not existing_park:
+        raise HTTPException(status_code=404, detail="Park not found")
+    return existing_park
+
+@app.delete("/park/{park_name}", response_model=schemas.Park)
+def delete_park(park_name: str, db: Session = Depends(get_db)):
+    park = crud.delete_park(db=db, park_name=park_name)
+    if not park:
+        raise HTTPException(status_code=404, detail="Park not found")
+    return park
