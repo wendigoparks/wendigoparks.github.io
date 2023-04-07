5b6b5de43b66eb62721f681aadf22c5b94eff9bb
brhuebsch <brhuebsch@vt.edu>
2023-04-07 16:23:45 -0400

    Add reservation joining in sql file

--- backend/parks.sql
+++ backend/parks.sql
@@ -23,6 +23,7 @@ CREATE TABLE IF NOT EXISTS facilities (
     longitude VARCHAR(16),
     description VARCHAR(512),
     phone_nr VARCHAR(32),
+    type VARCHAR(64) NOT NULL,
     capacity INT,
     image_url VARCHAR(256),
     park_id VARCHAR(36) NOT NULL,
@@ -35,7 +36,6 @@ CREATE TABLE IF NOT EXISTS courts (
     id VARCHAR(36) PRIMARY KEY,
     facility_id VARCHAR(36) NOT NULL,
     name VARCHAR(128) NOT NULL,
-    type VARCHAR(64) NOT NULL,
     reservable TINYINT DEFAULT false,
     FOREIGN KEY (facility_id)
         REFERENCES facilities(id)
@@ -59,10 +59,27 @@ CREATE TABLE IF NOT EXISTS reservations (
     end_time TIME NOT NULL,
     user_id VARCHAR(36) NOT NULL,
     court_id VARCHAR(36) NOT NULL,
+    group_size TINYINT,
+    number_can_join TINYINT default 0,
+    number_can_join_total TINYINT default 0,
     FOREIGN KEY (court_id)
         REFERENCES courts(id)
         ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (user_id)
         REFERENCES users(username)
-        ON UPDATE CASCADE ON DELETE CASCADE
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    INDEX(date),
+    INDEX(court_id, date)
+);
+
+CREATE TABLE IF NOT EXISTS reservations_joined (
+    id VARCHAR(36) PRIMARY KEY,
+    user_id VARCHAR(36) NOT NULL,
+    group_size TINYINT,
+    reservation_joined VARCHAR(36),
+    FOREIGN KEY (user_id)
+        REFERENCES users(username)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (reservation_joined)
+        REFERENCES reservations(id)
 );
